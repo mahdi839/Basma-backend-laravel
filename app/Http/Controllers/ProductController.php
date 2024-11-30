574000854b7a10fa273b71b5a51781a0d292fef4
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2024-12-01 00:40:11 +0600

    make api for storing images, sizes, faqs

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Size;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
@@ -19,7 +22,56 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+     $validated = $request->validate([
+
+            'title'=>'required',
+            'sub_title'=>'required',
+            'video_url'=>'nullable',
+            'description'=>'nullable',
+            'image'=>'required|array',
+            'image.*' => 'image|mimes:jpg,jpeg,png',
+            'sizes'=>'nullable|array',
+            'size_id' => 'required',
+            'price'=>'required|numeric',
+            'question'=>'nullable|array',
+            'answer'=>'nullable|array',
+
+         ]);
+
+         $product = Product::create([
+             'title'=>$validated['title'],
+             'sub_title'=>$validated['sub_title'],
+             'description'=>$validated['description']??null,
+             'video_url'=>$validated['video_url']??null,
+             'discount'=>$validated['discount']??null,
+         ]);
+
+
+         foreach($validated['image'] as $image){
+            $product->images()->create(['image'=>$image]);
+         }
+         
+          $allSizes =  Size::all();
+         foreach ($allSizes as $size){
+            $product->sizes()->attach($size->id,['price'=>$validated['price']]);
+         }
+
+
+         foreach ($validated['question'] as $key => $ques) {
+            $product->faqs()->create([
+                'question' => $ques,
+                'answer' => $validated['answer'][$key] ?? null,
+            ]);
+        }
+
+
+
+         return response()->json([
+             'message'=> 'product created successfully',
+             'data'=>$product->load('images','sizes','faqs')
+         ]);
+
     }
 
     /**
