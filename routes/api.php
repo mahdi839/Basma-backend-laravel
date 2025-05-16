318170ca76839b55cfabc272badc918cce05b083
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-05-17 00:40:25 +0600

    remove unwanted links from routes/api

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,15 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SizeController;
 use App\Http\Controllers\Api\CategoryController;
 
 
 
-Route::post('signUp',[AuthController::class,'signUp']);
-Route::post('logIn',[AuthController::class,'logIn']);
-Route::post('logOut',[AuthController::class,'logOut'])->middleware('auth:sanctum');
+Route::post('signUp', [AuthController::class, 'signUp']);
+Route::post('logIn', [AuthController::class, 'logIn']);
+Route::post('logOut', [AuthController::class, 'logOut'])->middleware('auth:sanctum');
 
 // Public routes (no auth needed)
 Route::apiResource('products', ProductController::class)
@@ -25,10 +23,8 @@ Route::apiResource('categories', CategoryController::class)->only(['index', 'sho
 Route::middleware(['auth:sanctum', 'isAdmin'])->group(function () {
     Route::apiResource('products', ProductController::class)
         ->only(['store', 'update', 'destroy']);
-        
+
     Route::apiResource('sizes', SizeController::class)
         ->only(['store', 'update', 'destroy']);
-        Route::apiResource('categories', CategoryController::class)->only(['store', 'update', 'destroy']);
+    Route::apiResource('categories', CategoryController::class)->only(['store', 'update', 'destroy']);
 });
-
-
