c3170a6cce90e5b935a32d3b20c5ed915c127caf
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-08-29 01:13:07 +0600

    make download csv api

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,111 +14,115 @@ class OrderController extends Controller
      */
     public function index(Request $request)
     {
-        $status = $request->query('status','');
-        $district = $request->query('district','');
-        $search = $request->input('search','');
-        $min = $request->query('min','');
-        $max = $request->query('max','');
-        $start_date = $request->query('start_date','');
-        $end_date = $request->query('end_date','');
-        $product_title = $request->query('product_title','');
+        $status = $request->query('status', '');
+        $district = $request->query('district', '');
+        $search = $request->input('search', '');
+        $min = $request->query('min', '');
+        $max = $request->query('max', '');
+        $start_date = $request->query('start_date', '');
+        $end_date = $request->query('end_date', '');
+        $product_title = $request->query('product_title', '');
 
 
         $orders = Order::with('orderItems')
-        ->when($status,function($q)use($status){
-            $q->where('status',$status);
-         })
-        ->when($district,function($q)use($district){
-           $q->where('district',$district);
-        })
-        ->when($min, function ($q) use ($min) {
-            $q->where('total', '>=', $min);
-        })
-        ->when($max, function ($q) use ($max) {
-            $q->where('total', '<=', $max);
-        })
-        ->when($start_date, function ($q) use ($start_date) {
-            $q->where('created_at', '>=', $start_date);
-        })
-        ->when($end_date, function ($q) use ($end_date) {
-            $q->where('created_at', '<=', $end_date);
-        })
-        ->when($product_title,function($q)use($product_title){
-            $q->whereHas('orderItems',function($query) use ($product_title){
-                $query->where('title',$product_title);
-            });
-        })
-        ->when($search, function ($q) use ($search) {
-            $q->where(function ($query) use ($search) {
-                $query->where('name', 'LIKE', "%$search%")
-                      ->orWhere('phone', 'LIKE', "%$search%");
-            });
-        })
-        ->paginate(10);
+            ->when($status, function ($q) use ($status) {
+                $q->where('status', $status);
+            })
+            ->when($district, function ($q) use ($district) {
+                $q->where('district', $district);
+            })
+            ->when($min, function ($q) use ($min) {
+                $q->where('total', '>=', $min);
+            })
+            ->when($max, function ($q) use ($max) {
+                $q->where('total', '<=', $max);
+            })
+            ->when($start_date, function ($q) use ($start_date) {
+                $q->where('created_at', '>=', $start_date);
+            })
+            ->when($end_date, function ($q) use ($end_date) {
+                $q->where('created_at', '<=', $end_date);
+            })
+            ->when($product_title, function ($q) use ($product_title) {
+                $q->whereHas('orderItems', function ($query) use ($product_title) {
+                    $query->where('title', $product_title);
+                });
+            })
+            ->when($search, function ($q) use ($search) {
+                $q->where(function ($query) use ($search) {
+                    $query->where('name', 'LIKE', "%$search%")
+                        ->orWhere('phone', 'LIKE', "%$search%");
+                });
+            })
+            ->paginate(10);
         return response()->json($orders);
     }
 
-    public function downloadCSV (Request $request){
-            $status = $request->query('status','');
-        $district = $request->query('district','');
-        $search = $request->input('search','');
-        $min = $request->query('min','');
-        $max = $request->query('max','');
-        $start_date = $request->query('start_date','');
-        $end_date = $request->query('end_date','');
-        $product_title = $request->query('product_title','');
+    public function downloadCSV(Request $request)
+    {
+        $status = $request->query('status', '');
+        $district = $request->query('district', '');
+        $search = $request->input('search', '');
+        $min = $request->query('min', '');
+        $max = $request->query('max', '');
+        $start_date = $request->query('start_date', '');
+        $end_date = $request->query('end_date', '');
+        $product_title = $request->query('product_title', '');
         $orders = Order::with('orderItems')
-        ->when($status,function($q)use($status){
-            $q->where('status',$status);
-         })
-        ->when($district,function($q)use($district){
-           $q->where('district',$district);
-        })
-        ->when($min, function ($q) use ($min) {
-            $q->where('total', '>=', $min);
-        })
-        ->when($max, function ($q) use ($max) {
-            $q->where('total', '<=', $max);
-        })
-        ->when($start_date, function ($q) use ($start_date) {
-            $q->where('created_at', '>=', $start_date);
-        })
-        ->when($end_date, function ($q) use ($end_date) {
-            $q->where('created_at', '<=', $end_date);
-        })
-        ->when($product_title,function($q)use($product_title){
-            $q->whereHas('orderItems',function($query) use ($product_title){
-                $query->where('title',$product_title);
-            });
-        })
-        ->when($search, function ($q) use ($search) {
-            $q->where(function ($query) use ($search) {
-                $query->where('name', 'LIKE', "%$search%")
-                      ->orWhere('phone', 'LIKE', "%$search%");
-            });
-        })->get();
-
+            ->when($status, function ($q) use ($status) {
+                $q->where('status', $status);
+            })
+            ->when($district, function ($q) use ($district) {
+                $q->where('district', $district);
+            })
+            ->when($min, function ($q) use ($min) {
+                $q->where('total', '>=', $min);
+            })
+            ->when($max, function ($q) use ($max) {
+                $q->where('total', '<=', $max);
+            })
+            ->when($start_date, function ($q) use ($start_date) {
+                $q->where('created_at', '>=', $start_date);
+            })
+            ->when($end_date, function ($q) use ($end_date) {
+                $q->where('created_at', '<=', $end_date);
+            })
+            ->when($product_title, function ($q) use ($product_title) {
+                $q->whereHas('orderItems', function ($query) use ($product_title) {
+                    $query->where('title', $product_title);
+                });
+            })
+            ->when($search, function ($q) use ($search) {
+                $q->where(function ($query) use ($search) {
+                    $query->where('name', 'LIKE', "%$search%")
+                        ->orWhere('phone', 'LIKE', "%$search%");
+                });
+            })->get();
 
         $date = Date('Y-m-d');
         $fileName = "orders_{$date}.csv";
 
         $header = [
-            'content-type'=>'text/csv',
-            'content-disposition'=> "attachment; filename={$fileName}",
+            'content-type' => 'text/csv',
+            'content-disposition' => "attachment; filename={$fileName}",
         ];
-        
-        $columns = ["Customer Info","Ordered Products","Order Summery","status"];
-        $callback = function () use ($orders,$columns){
-           $file = fopen('php://output','w');
-           fputcsv($file,$columns);
 
-           foreach($orders as $order){
-             $customer_info = "{$order->name}| {$order->phone}| {$order->district}";
-           }
+        $columns = ["Customer Info", "Ordered Products", "Order Summery", "status"];
+        $callback = function () use ($orders, $columns) {
+            $file = fopen('php://output', 'w');
+            fputcsv($file, $columns);
+
+            foreach ($orders as $order) {
+                $customer_info = "{$order->name}| {$order->phone}| {$order->district}";
+                $ordered_products = $order->orderItems->map(fn($item) => "{$item->title} ({$item->qty})")->implode("; ");
+                $orderSummary = "Total: {$order->total}";
+                $status = $order->status;
+                fputcsv($file, [$customer_info, $ordered_products, $orderSummary, $status]);
+            }
+            fclose($file);
         };
-    
 
-
+        return response()->stream($callback,200,$header);
     }
 
     /**
@@ -140,8 +144,8 @@ class OrderController extends Controller
             'phone' => 'nullable|string',
             'shipping_cost' => 'required|numeric',
             'district' => 'required|string',
-            'address'=>'required|string',
-            'delivery_notes'=>'nullable|string',
+            'address' => 'required|string',
+            'delivery_notes' => 'nullable|string',
             'payment_method' => 'required|string',
             'cart' => 'required|array',
             'cart.*.id' => 'required|integer',
@@ -151,13 +155,13 @@ class OrderController extends Controller
             'cart.*.qty' => 'required|integer',
             'cart.*.totalPrice' => 'required|numeric',
         ]);
-    
+
         // Calculate totals
         $subtotal = collect($request->cart)->sum('totalPrice');
         $total = $subtotal - $request->shipping_cost; // Add shipping/tax if needed
 
-       
-    
+
+
         // Create order
         $order = Order::create([
             'order_number' => 'ORD-' . date('Ymd') . '-' . strtoupper(uniqid()),
@@ -167,17 +171,17 @@ class OrderController extends Controller
             'district' => $request->district,
             'subtotal' => $subtotal,
             'total' => $total,
-            'shipping_cost'=>$request->shipping_cost,
-            'delivery_notes'=>$request->delivery_notes,
-            'status'=> 'placed',
+            'shipping_cost' => $request->shipping_cost,
+            'delivery_notes' => $request->delivery_notes,
+            'status' => 'placed',
             'payment_method' => $request->payment_method,
         ]);
-    
+
         // Create order items
         foreach ($request->cart as $item) {
             OrderItem::create([
                 'order_id' => $order->id,
-                'product_id'=>$item['id'],
+                'product_id' => $item['id'],
                 'title' => $item['title'],
                 'size' => $item['size'],
                 'unitPrice' => $item['unitPrice'],
@@ -185,7 +189,7 @@ class OrderController extends Controller
                 'totalPrice' => $item['totalPrice'],
             ]);
         }
-    
+
         return response()->json([
             'message' => 'Order created successfully',
             'order_number' => $order->order_number
