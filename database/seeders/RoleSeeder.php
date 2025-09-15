9846a700e67bb447e8a0a0c31e22ece361dee8ac
Mehedi Hasan <hasanarefi56574@gmail.com>
2025-09-15 12:58:44 +0600

    modify seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -1,37 +1,38 @@
 <?php
 
 namespace Database\Seeders;
-use Illuminate\Support\Str;
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
-class roleSeeder extends Seeder
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class RoleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-         $admins = [
+        $admins = [
             [
                 'name' => 'Mehedi',
                 'email' => 'hasanarefi56574@gmail.com',
-                'password' => bcrypt('2443424434'),
+                'password' => '2443424434',
             ],
             [
-                'name' => 'mehedi@gmail.com',
+                'name' => 'Mehedi Admin',
                 'email' => 'mehedi@gmail.com',
-                'password' => bcrypt('mehedi@gmail.com'),
+                'password' => 'mehedi@gmail.com',
             ],
         ];
 
         foreach ($admins as $admin) {
             User::updateOrCreate(
-                ['email' => $admin['email']], // Check existing by email
+                ['email' => $admin['email']],
                 [
                     'name' => $admin['name'],
-                    'password' => $admin['password'],
+                    'password' => Hash::make($admin['password']), // hash plain text here
                     'role' => 'admin',
                     'remember_token' => Str::random(10),
                     'created_at' => now(),
@@ -39,7 +40,5 @@ class roleSeeder extends Seeder
                 ]
             );
         }
-
-
     }
 }
