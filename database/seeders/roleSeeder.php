edfd41d7808204a3e9c358e20d4bc00000c5b39f
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-09-19 00:11:12 +0600

    made hash

--- database/seeders/roleSeeder.php
+++ database/seeders/roleSeeder.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Str;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class roleSeeder extends Seeder
 {
@@ -16,7 +17,7 @@ class roleSeeder extends Seeder
         $user = User::create([
             'name' => 'mehedi',
             'email' => 'hasanarefi56574@gmail.com',
-            'password' => bcrypt('2443424434'), // Always encrypt passwords
+            'password' => Hash::make('2443424434'), // Always encrypt passwords
             'role' => 'admin',
             'remember_token' => str::random(10),
             'created_at' => now(),
