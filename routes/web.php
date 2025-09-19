8d7c3735e53c05b73bafef91a03a4f61e65b5d15
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-09-20 02:35:14 +0600

    used and showd admin lte dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\web\DashboardController\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',[HomeController::class,'index']);
