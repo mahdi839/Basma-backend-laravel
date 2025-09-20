c816f8db003d4ef3014c1ecaa6ba882f21991b3a
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-09-20 23:05:59 +0600

    removed unwanted things

--- resources/views/layouts/dashboard/layout.blade.php
+++ resources/views/layouts/dashboard/layout.blade.php
@@ -86,7 +86,7 @@
     <!--end::App Wrapper-->
     <!--begin::Script-->
     <!--begin::Third Party Plugin(OverlayScrollbars)-->
-    <script
+<script
       src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/browser/overlayscrollbars.browser.es6.min.js"
       crossorigin="anonymous"
     ></script>
@@ -125,191 +125,121 @@
     </script>
     <!--end::OverlayScrollbars Configure-->
     <!-- OPTIONAL SCRIPTS -->
-    <!-- sortablejs -->
-    <script
-      src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"
-      crossorigin="anonymous"
-    ></script>
-    <!-- sortablejs -->
-    <script>
-      new Sortable(document.querySelector('.connectedSortable'), {
-        group: 'shared',
-        handle: '.card-header',
-      });
-
-      const cardHeaders = document.querySelectorAll('.connectedSortable .card-header');
-      cardHeaders.forEach((cardHeader) => {
-        cardHeader.style.cursor = 'move';
-      });
-    </script>
     <!-- apexcharts -->
     <script
       src="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.min.js"
       integrity="sha256-+vh8GkaU7C9/wbSLIcwq82tQ2wTf44aOHA8HlBMwRI8="
       crossorigin="anonymous"
     ></script>
-    <!-- ChartJS -->
     <script>
       // NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
       // IT'S ALL JUST JUNK FOR DEMO
       // ++++++++++++++++++++++++++++++++++++++++++
 
-      const sales_chart_options = {
+      const visitors_chart_options = {
         series: [
           {
-            name: 'Digital Goods',
-            data: [28, 48, 40, 19, 86, 27, 90],
+            name: 'High - 2023',
+            data: [100, 120, 170, 167, 180, 177, 160],
           },
           {
-            name: 'Electronics',
-            data: [65, 59, 80, 81, 56, 55, 40],
+            name: 'Low - 2023',
+            data: [60, 80, 70, 67, 80, 77, 100],
           },
         ],
         chart: {
-          height: 300,
-          type: 'area',
+          height: 200,
+          type: 'line',
           toolbar: {
             show: false,
+          },
+        },
+        colors: ['#0d6efd', '#adb5bd'],
+        stroke: {
+          curve: 'smooth',
+        },
+        grid: {
+          borderColor: '#e7e7e7',
+          row: {
+            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            opacity: 0.5,
           },
         },
         legend: {
           show: false,
         },
-        colors: ['#0d6efd', '#20c997'],
+        markers: {
+          size: 1,
+        },
+        xaxis: {
+          categories: ['22th', '23th', '24th', '25th', '26th', '27th', '28th'],
+        },
+      };
+
+      const visitors_chart = new ApexCharts(
+        document.querySelector('#visitors-chart'),
+        visitors_chart_options,
+      );
+      visitors_chart.render();
+
+      const sales_chart_options = {
+        series: [
+          {
+            name: 'Net Profit',
+            data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+          },
+          {
+            name: 'Revenue',
+            data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+          },
+          {
+            name: 'Free Cash Flow',
+            data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+          },
+        ],
+        chart: {
+          type: 'bar',
+          height: 200,
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded',
+          },
+        },
+        legend: {
+          show: false,
+        },
+        colors: ['#0d6efd', '#20c997', '#ffc107'],
         dataLabels: {
           enabled: false,
         },
         stroke: {
-          curve: 'smooth',
+          show: true,
+          width: 2,
+          colors: ['transparent'],
         },
         xaxis: {
-          type: 'datetime',
-          categories: [
-            '2023-01-01',
-            '2023-02-01',
-            '2023-03-01',
-            '2023-04-01',
-            '2023-05-01',
-            '2023-06-01',
-            '2023-07-01',
-          ],
+          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        },
+        fill: {
+          opacity: 1,
         },
         tooltip: {
-          x: {
-            format: 'MMMM yyyy',
+          y: {
+            formatter: function (val) {
+              return '$ ' + val + ' thousands';
+            },
           },
         },
       };
 
       const sales_chart = new ApexCharts(
-        document.querySelector('#revenue-chart'),
+        document.querySelector('#sales-chart'),
         sales_chart_options,
       );
       sales_chart.render();
-    </script>
-    <!-- jsvectormap -->
-    <script
-      src="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/js/jsvectormap.min.js"
-      integrity="sha256-/t1nN2956BT869E6H4V1dnt0X5pAQHPytli+1nTZm2Y="
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/maps/world.js"
-      integrity="sha256-XPpPaZlU8S/HWf7FZLAncLg2SAkP8ScUTII89x9D3lY="
-      crossorigin="anonymous"
-    ></script>
-    <!-- jsvectormap -->
-    <script>
-      // World map by jsVectorMap
-      new jsVectorMap({
-        selector: '#world-map',
-        map: 'world',
-      });
-
-      // Sparkline charts
-      const option_sparkline1 = {
-        series: [
-          {
-            data: [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021],
-          },
-        ],
-        chart: {
-          type: 'area',
-          height: 50,
-          sparkline: {
-            enabled: true,
-          },
-        },
-        stroke: {
-          curve: 'straight',
-        },
-        fill: {
-          opacity: 0.3,
-        },
-        yaxis: {
-          min: 0,
-        },
-        colors: ['#DCE6EC'],
-      };
-
-      const sparkline1 = new ApexCharts(document.querySelector('#sparkline-1'), option_sparkline1);
-      sparkline1.render();
-
-      const option_sparkline2 = {
-        series: [
-          {
-            data: [515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921],
-          },
-        ],
-        chart: {
-          type: 'area',
-          height: 50,
-          sparkline: {
-            enabled: true,
-          },
-        },
-        stroke: {
-          curve: 'straight',
-        },
-        fill: {
-          opacity: 0.3,
-        },
-        yaxis: {
-          min: 0,
-        },
-        colors: ['#DCE6EC'],
-      };
-
-      const sparkline2 = new ApexCharts(document.querySelector('#sparkline-2'), option_sparkline2);
-      sparkline2.render();
-
-      const option_sparkline3 = {
-        series: [
-          {
-            data: [15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21],
-          },
-        ],
-        chart: {
-          type: 'area',
-          height: 50,
-          sparkline: {
-            enabled: true,
-          },
-        },
-        stroke: {
-          curve: 'straight',
-        },
-        fill: {
-          opacity: 0.3,
-        },
-        yaxis: {
-          min: 0,
-        },
-        colors: ['#DCE6EC'],
-      };
-
-      const sparkline3 = new ApexCharts(document.querySelector('#sparkline-3'), option_sparkline3);
-      sparkline3.render();
     </script>
     <!--end::Script-->
   </body>
