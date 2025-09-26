be878b04610bc0cf2895ab7fe5bd263a1dd43414
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-09-26 15:37:56 +0600

    hide timestamp

--- app/Models/Size.php
+++ app/Models/Size.php
@@ -8,6 +8,7 @@ class Size extends Model
 {
     //
       protected $guarded = [];
+      protected $hidden = ['created_at', 'updated_at']; // <- add this
     public function products()
     {
         return $this->belongsToMany(Product::class, 'product_sizes')
