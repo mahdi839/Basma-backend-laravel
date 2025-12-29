fac5c9d48f7713b1bbe543224202d1655148ba1e
mahdi839 <hasanarefi56574@gmail.com>
2025-12-29 23:56:14 +0600

    add sku column on product table and auto generate sku when create product

--- database/migrations/2024_11_28_165730_create_products_table.php
+++ database/migrations/2024_11_28_165730_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('sku')->unique();
             $table->text('short_description');
             $table->longText('description')->nullable();
             $table->integer('price')->nullable();
